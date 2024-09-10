e0809864d56118fa4bbad6d56fc4a4834f6a73fa
s3xmaxman <badside3919@gmail.com>
2024-09-10 10:53:47 +0900

    update book-ride.tsx and Payment.tsx

--- app/(root)/book-ride.tsx
+++ app/(root)/book-ride.tsx
@@ -1,5 +1,6 @@
 import { useUser } from "@clerk/clerk-expo";
 import { Image, Text, View } from "react-native";
+import { StripeProvider } from "@stripe/stripe-react-native";
 
 import RideLayout from "@/components/RideLayout";
 import { icons } from "@/constants";
@@ -17,77 +18,83 @@ const BookRide = () => {
   )[0];
 
   return (
-    <RideLayout title="Book Ride">
-      <>
-        <Text className="text-xl font-JakartaSemiBold mb-3">
-          ドライバーの情報
-        </Text>
+    <StripeProvider
+      publishableKey={process.env.EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY!}
+      merchantIdentifier="merchant.com.uber"
+      urlScheme="myapp"
+    >
+      <RideLayout title="Book Ride">
+        <>
+          <Text className="text-xl font-JakartaSemiBold mb-3">
+            ドライバーの情報
+          </Text>
 
-        <View className="flex flex-col w-full items-center justify-center mt-10">
-          <Image
-            source={{ uri: driverDetails?.profile_image_url }}
-            className="w-28 h-28 rounded-full"
-          />
+          <View className="flex flex-col w-full items-center justify-center mt-10">
+            <Image
+              source={{ uri: driverDetails?.profile_image_url }}
+              className="w-28 h-28 rounded-full"
+            />
 
-          <View className="flex flex-row items-center justify-center mt-5 space-x-2">
-            <Text className="text-lg font-JakartaSemiBold">
-              {driverDetails?.title}
-            </Text>
+            <View className="flex flex-row items-center justify-center mt-5 space-x-2">
+              <Text className="text-lg font-JakartaSemiBold">
+                {driverDetails?.title}
+              </Text>
 
-            <View className="flex flex-row items-center space-x-0.5">
-              <Image
-                source={icons.star}
-                className="w-5 h-5"
-                resizeMode="contain"
-              />
+              <View className="flex flex-row items-center space-x-0.5">
+                <Image
+                  source={icons.star}
+                  className="w-5 h-5"
+                  resizeMode="contain"
+                />
+                <Text className="text-lg font-JakartaRegular">
+                  {driverDetails?.rating}
+                </Text>
+              </View>
+            </View>
+          </View>
+
+          <View className="flex flex-col w-full items-start justify-center py-3 px-5 rounded-3xl bg-general-600 mt-5">
+            <View className="flex flex-row items-center justify-between w-full border-b border-white py-3">
+              <Text className="text-lg font-JakartaRegular">料金</Text>
+              <Text className="text-lg font-JakartaRegular text-[#0CC25F]">
+                ${driverDetails?.price}
+              </Text>
+            </View>
+
+            <View className="flex flex-row items-center justify-between w-full border-b border-white py-3">
+              <Text className="text-lg font-JakartaRegular">乗車時間</Text>
               <Text className="text-lg font-JakartaRegular">
-                {driverDetails?.rating}
+                {formatTime(driverDetails?.time!)}
+              </Text>
+            </View>
+
+            <View className="flex flex-row items-center justify-between w-full py-3">
+              <Text className="text-lg font-JakartaRegular">座席数</Text>
+              <Text className="text-lg font-JakartaRegular">
+                {driverDetails?.car_seats}
               </Text>
             </View>
           </View>
-        </View>
 
-        <View className="flex flex-col w-full items-start justify-center py-3 px-5 rounded-3xl bg-general-600 mt-5">
-          <View className="flex flex-row items-center justify-between w-full border-b border-white py-3">
-            <Text className="text-lg font-JakartaRegular">料金</Text>
-            <Text className="text-lg font-JakartaRegular text-[#0CC25F]">
-              ${driverDetails?.price}
-            </Text>
-          </View>
+          <View className="flex flex-col w-full items-start justify-center mt-5">
+            <View className="flex flex-row items-center justify-start mt-3 border-t border-b border-general-700 w-full py-3">
+              <Image source={icons.to} className="w-6 h-6" />
+              <Text className="text-lg font-JakartaRegular ml-2">
+                {userAddress}
+              </Text>
+            </View>
 
-          <View className="flex flex-row items-center justify-between w-full border-b border-white py-3">
-            <Text className="text-lg font-JakartaRegular">乗車時間</Text>
-            <Text className="text-lg font-JakartaRegular">
-              {formatTime(driverDetails?.time!)}
-            </Text>
+            <View className="flex flex-row items-center justify-start border-b border-general-700 w-full py-3">
+              <Image source={icons.point} className="w-6 h-6" />
+              <Text className="text-lg font-JakartaRegular ml-2">
+                {destinationAddress}
+              </Text>
+            </View>
           </View>
-
-          <View className="flex flex-row items-center justify-between w-full py-3">
-            <Text className="text-lg font-JakartaRegular">座席数</Text>
-            <Text className="text-lg font-JakartaRegular">
-              {driverDetails?.car_seats}
-            </Text>
-          </View>
-        </View>
-
-        <View className="flex flex-col w-full items-start justify-center mt-5">
-          <View className="flex flex-row items-center justify-start mt-3 border-t border-b border-general-700 w-full py-3">
-            <Image source={icons.to} className="w-6 h-6" />
-            <Text className="text-lg font-JakartaRegular ml-2">
-              {userAddress}
-            </Text>
-          </View>
-
-          <View className="flex flex-row items-center justify-start border-b border-general-700 w-full py-3">
-            <Image source={icons.point} className="w-6 h-6" />
-            <Text className="text-lg font-JakartaRegular ml-2">
-              {destinationAddress}
-            </Text>
-          </View>
-        </View>
-        <Payment />
-      </>
-    </RideLayout>
+          <Payment />
+        </>
+      </RideLayout>
+    </StripeProvider>
   );
 };
 
--- components/Payment.tsx
+++ components/Payment.tsx
@@ -1,8 +1,26 @@
-import { View, Text } from "react-native";
-import React from "react";
-import CustomButton from "./CustomButton";
+import { useAuth } from "@clerk/clerk-expo";
+import { useStripe } from "@stripe/stripe-react-native";
+import { router } from "expo-router";
+import React, { useState } from "react";
+import { Alert, Image, Text, View } from "react-native";
+import { ReactNativeModal } from "react-native-modal";
+
+import CustomButton from "@/components/CustomButton";
+import { images } from "@/constants";
+import { fetchAPI } from "@/lib/fetch";
+import { useLocationStore } from "@/store";
+import { PaymentProps } from "@/types/type";
 
 const Payment = () => {
+  const { initPaymentSheet, presentPaymentSheet } = useStripe();
+  const {
+    userAddress,
+    userLongitude,
+    userLatitude,
+    destinationLatitude,
+    destinationAddress,
+    destinationLongitude,
+  } = useLocationStore();
   const openPaymentSheet = () => {};
   return (
     <>
