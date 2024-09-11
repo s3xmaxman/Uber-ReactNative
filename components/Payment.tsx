67be1b5ef8097dc6c0e6a661f5aa39a19dcb62b4
s3xmaxman <badside3919@gmail.com>
2024-09-11 10:01:42 +0900

    update home.tsx and Payment.tsx

--- components/Payment.tsx
+++ components/Payment.tsx
@@ -45,7 +45,7 @@ const Payment = ({
 
   const initializePaymentSheet = async () => {
     const { error } = await initPaymentSheet({
-      merchantDisplayName: "Ryde",
+      merchantDisplayName: "Example, Inc.",
       intentConfiguration: {
         mode: {
           amount: parseInt(amount) * 100,
@@ -67,7 +67,7 @@ const Payment = ({
                 name: fullName || email.split("@")[0],
                 email: email,
                 amount: amount,
-                paymentMethod: paymentMethod.id,
+                paymentMethodId: paymentMethod.id,
               }),
             }
           );
@@ -87,12 +87,39 @@ const Payment = ({
             });
 
             if (result.client_secret) {
-              // TODO: add ride create
+              await fetchAPI("/(api)/ride/create", {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                  origin_address: userAddress,
+                  destination_address: destinationAddress,
+                  origin_latitude: userLatitude,
+                  origin_longitude: userLongitude,
+                  destination_latitude: destinationLatitude,
+                  destination_longitude: destinationLongitude,
+                  ride_time: rideTime.toFixed(0),
+                  fare_price: parseInt(amount) * 100,
+                  payment_status: "paid",
+                  driver_id: driverId,
+                  user_id: userId,
+                }),
+              });
+
+              intentCreationCallback({
+                clientSecret: result.client_secret,
+              });
             }
           }
         },
       },
+      returnURL: "myapp://book-ride",
     });
+
+    if (!error) {
+      // setLoading(true);
+    }
   };
 
   return (
@@ -102,6 +129,31 @@ const Payment = ({
         className="my-10"
         onPress={openPaymentSheet}
       />
+
+      <ReactNativeModal
+        isVisible={success}
+        onBackdropPress={() => setSuccess(false)}
+      >
+        <View className="flex flex-col items-center justify-center bg-white p-7 rounded-2xl">
+          <Image source={images.check} className="w-28 h-28 mt-5" />
+          <Text className="text-2xl text-center font-JakartaBold mt-5">
+            予約を確定しました
+          </Text>
+          <Text className="text-md text-general-200 font-JakartaRegular text-center mt-3 ">
+            ご予約ありがとうございます。予約が完了しました。
+            ドライバーがお迎えに伺いますので、 しばらくお待ちください。
+          </Text>
+
+          <CustomButton
+            title="ホームへ"
+            className="mt-5"
+            onPress={() => {
+              setSuccess(false);
+              router.push("/(root)/(tabs)/home");
+            }}
+          />
+        </View>
+      </ReactNativeModal>
     </>
   );
 };
